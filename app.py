86842d7407366b7be17bf95f0bd3aaba92775117
kgagne15 <kristinagagne@ymail.com>
2022-07-31 13:50:16 -0400

    Part 1 complete

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, redirect, session, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import connect_db, db, User, Tweet
+from forms import UserForm, TweetForm
+from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///flask_feedback"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "321cba"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
---
+++ models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User model
+    username: unique PK String(20)
+    password: not null Text
+    email: not null, unique, String(50)
+    first_name: not null, String(30)
+    last_name: not null String(30)
+    """
+
+    __tablename__ = "users"
+
+    username = db.Column(db.String(20), primary_key=True, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    
